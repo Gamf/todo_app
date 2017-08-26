b8fa45cf629039a45cafd349cd4aac170aada0c8
odoo <odoo@odoo10.moiro.org>
2017-08-26 18:09:50 +0300

    todo app start

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name' : 'To-Do Application',
+	'description' : 'Manage your personal To-Do tasks.',
+	'author' : 'Igor',
+	'depends' : ['base'],
+	'application' : True,
+	'data' : ['views/todo_menu.xml'],
+}
