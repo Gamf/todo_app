492a118d5c5d588d1311bb0ccc46be02f0b5f52d
odoo <odoo@odoo10.moiro.org>
2017-08-29 16:26:23 +0300

    clear all done button not working

--- todo_model.py
+++ todo_model.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*- 
-from odoo import models, fields
+from odoo import models, fields, api
 class TodoTask(models.Model):
 	_name = 'todo.task'
 	_description = 'To-do Task'
 	name = fields.Char('Description', required = True)
 	is_done = fields.Boolean('Done?')
 	active = fields.Boolean('Active', default = True)
+
+	@api.multi
+	def do_toggle_done(self):
+		for task in self:
+			task.is_done = not task.is_done
+		return True
+
+	@api.model
+	#@api.multi
+	def do_clear_done(self):
+		dones = self.search([('is_done', '=', True)])
+		dones.write({'active': False})
+		#for task in self:
+		#	task.active = False
+			#task.is_done = False
+		return True
+
+
 
